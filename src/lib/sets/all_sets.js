a322496aa88b15bba4af8ef9e99cbdf62302a52f
Zhichen Guo <zg63@duke.edu>
2025-01-29 00:08:01 -0500

    circle of 5ths flashcards

--- src/lib/sets/all_sets.js
+++ src/lib/sets/all_sets.js
@@ -1,4 +1,5 @@
 import { animals, animals_in_spanish } from './example-sets.js';
+import circle_of_fifths from './circle-of-fifths.js';
 
 export const names_to_slug = [
     {
@@ -8,12 +9,17 @@ export const names_to_slug = [
     {
         name: "Animals",
         slug: "animals"
+    },
+    {
+        name: "Circle of Fifths",
+        slug: "circle-of-fifths"
     }
 ]
 
 const slugs_to_set = {
     "animals-in-spanish": animals_in_spanish,
-    "animals": animals
+    "animals": animals,
+    "circle-of-fifths": circle_of_fifths
 }
 
 export function get_set(name) {
