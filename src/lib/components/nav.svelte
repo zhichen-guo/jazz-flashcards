fef39493c4f57ac7d4c56a94c17e64046838916e
Zhichen Guo <zg63@duke.edu>
2025-01-30 00:51:01 -0500

    placeholders

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -20,8 +20,8 @@
         </div>
     {/if}
     <div class="grid grid-cols-3 gap-x-0.5 col-start-2 justify-self-center">
-        <button class="py-2 px-4 rounded-l-md {selected === "cards" ? "bg-zinc-400" : "bg-zinc-500 hover:bg-zinc-400"}">cards</button>
-        <button class="py-2 px-4 {selected === "combo" ? "bg-zinc-400" : "bg-zinc-500 hover:bg-zinc-400"}">combo</button>
-        <button class="py-2 px-4 rounded-r-md {selected === "piano" ? "bg-zinc-400" : "bg-zinc-500 hover:bg-zinc-400"}">piano</button>
+        <button onclick={() => {goto("/flashcards")}} class="py-2 px-4 rounded-l-md {selected === "cards" ? "bg-zinc-400" : "bg-zinc-500 hover:bg-zinc-400"}">cards</button>
+        <button onclick={() => {goto("/combo")}} class="py-2 px-4 {selected === "combo" ? "bg-zinc-400" : "bg-zinc-500 hover:bg-zinc-400"}">combo</button>
+        <button onclick={() => {goto("/piano")}} class="py-2 px-4 rounded-r-md {selected === "piano" ? "bg-zinc-400" : "bg-zinc-500 hover:bg-zinc-400"}">piano</button>
     </div>
 </div>
